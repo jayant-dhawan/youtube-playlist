7eaf91a372931d9d35678b97a6e8fe1f3c3aac89
Jayant <jayantdhawan95@gmail.com>
2020-07-11 04:54:26 +0530

    deploy

---
+++ precache-manifest.b2fbf9004c7f37c74add56c2ad82921e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc26ea7963de77ced04347f3abee9d47",
+    "url": "./index.html"
+  },
+  {
+    "revision": "066198ca75f2fd1b8a4c",
+    "url": "./static/css/main.9d1f3529.chunk.css"
+  },
+  {
+    "revision": "0e0ca6d9eecddc1eb657",
+    "url": "./static/js/2.cc46d60e.chunk.js"
+  },
+  {
+    "revision": "8a9adb48f7af2062183ac70b4922c4ef",
+    "url": "./static/js/2.cc46d60e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "066198ca75f2fd1b8a4c",
+    "url": "./static/js/main.0e5d91a0.chunk.js"
+  },
+  {
+    "revision": "84270e23c8d48875db78",
+    "url": "./static/js/runtime-main.d361adfd.js"
+  }
+]);
